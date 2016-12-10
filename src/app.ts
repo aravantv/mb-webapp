4546bc354cfe09109775516fbc1d56bade6be6f4
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2016-12-10 07:34:17 +0100

    clean up before switching to elm

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import { ListWithUniqueSelection } from './listWithUniqueSelection'
-import { SelectableText } from './selectableText'
-import { makeDOMDriver } from '@cycle/dom'
-import { run } from '@cycle/xstream-run'
-
-run(ListWithUniqueSelection, {
-  dom: makeDOMDriver('#app'),
-})
