e66528f2d64477c8a4cd6ff7a1d1051bd1cf039a
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2016-12-24 10:13:13 +0100

    propagates new widget structure to Main, temporarily makes Main use only text and not list

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,39 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html
-import SelectableList
+
+
+{--import SelectableList
+import NewText--}
+
 import SelectableText
+import Widget exposing (toConcreteWidget, toConcreteWidgetWithFlags)
 
 
-main : Program Never (SelectableList.Model SelectableText.Model) (SelectableList.Msg SelectableText.Msg)
+type alias DataModel =
+    { tasks : List String }
+
+
+
+{--main : Program Never (SelectableList.Model SelectableText.Model) (SelectableList.Msg NewText.Msg SelectableText.Msg)
 main =
-    Html.program (SelectableList.createListWidget SelectableText.widget)
+    Html.program (SelectableList.createListWidget NewText.widget SelectableText.widget)
+        Html.program--}
+
+
+port getValue : (String -> msg) -> Sub msg
+
+
+port setValue : String -> Cmd msg
+
+
+textBinding : Widget.Binding msg String
+textBinding =
+    { get = getValue identity
+    , set = setValue
+    }
+
+
+main : Program Never SelectableText.Model SelectableText.Msg
+main =
+    Html.program (toConcreteWidget <| SelectableText.createWidget textBinding)
