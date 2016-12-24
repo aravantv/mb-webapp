83f9574f62ce04612a9ccaadfc9c2c8c3ff9cfe8
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2016-12-24 10:11:06 +0100

    own module for widgets

---
+++ src/Widget.elm
@@ -0,0 +1,92 @@
+module Widget exposing (..)
+
+import Html exposing (Html)
+import Task
+
+
+type alias AbstractWidget model msg base =
+    { base
+        | init : ( model, Cmd msg )
+        , update : msg -> model -> ( model, Cmd msg )
+        , subscriptions : model -> Sub msg
+        , view : model -> Html msg
+    }
+
+
+type alias Widget model msg =
+    AbstractWidget model msg {}
+
+
+toConcreteWidget : AbstractWidget model msg base -> Widget model msg
+toConcreteWidget widget =
+    { init = widget.init, update = widget.update, subscriptions = widget.subscriptions, view = widget.view }
+
+
+toConcreteWidgetWithFlags :
+    AbstractWidget model msg base
+    -> { init : flags -> ( model, Cmd msg )
+       , update : msg -> model -> ( model, Cmd msg )
+       , subscriptions : model -> Sub msg
+       , view : model -> Html msg
+       }
+toConcreteWidgetWithFlags widget =
+    { init = \_ -> widget.init, update = widget.update, subscriptions = widget.subscriptions, view = widget.view }
+
+
+wrapUpdateWithCmd : (msg -> model -> model) -> msg -> model -> ( model, Cmd msg )
+wrapUpdateWithCmd update =
+    \msg model -> update msg model ! []
+
+
+wrapModelWithCmd : model -> ( model, Cmd msg )
+wrapModelWithCmd model =
+    ( model, Cmd.none )
+
+
+cmdOfMsg : msg -> Cmd msg
+cmdOfMsg msg =
+    Task.perform identity (Task.succeed msg)
+
+
+emptySubscription : model -> Sub msg
+emptySubscription _ =
+    Sub.none
+
+
+type alias ISelectable model msg base =
+    { base
+        | isSelected : model -> Bool
+        , selectMsg : msg
+        , unselectMsg : msg
+    }
+
+
+type alias Selectable model msg =
+    ISelectable model msg (Widget model msg)
+
+
+type alias IDecision msg base =
+    { base
+        | confirmMsg : msg
+        , cancelMsg : msg
+    }
+
+
+type alias DecisionWidget model msg =
+    IDecision msg (Widget model msg)
+
+
+type alias Binding msg ty =
+    { get : Sub ty
+    , set : ty -> Cmd msg
+    }
+
+
+type alias Index =
+    Int
+
+
+type alias ListBinding msg ty =
+    { get : Sub msg
+    , setItem : Index -> ty -> Cmd msg
+    }
