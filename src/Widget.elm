b1c12d8c0f708e856c81260c371bff7f37116154
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-01-21 00:24:13 +0100

    adds binding to widgets

--- src/Widget.elm
+++ src/Widget.elm
@@ -4,33 +4,64 @@ import Html exposing (Html)
 import Task
 
 
-type alias AbstractWidget model msg base =
-    { base
-        | init : ( model, Cmd msg )
-        , update : msg -> model -> ( model, Cmd msg )
-        , subscriptions : model -> Sub msg
-        , view : model -> Html msg
-    }
+-- Row types are used only to compose new types in a modular fashion,
+-- *not* to make functions accept abstract structures:
+-- just use wrappers if you do not have the precise concrete type
 
 
 type alias Widget model msg =
-    AbstractWidget model msg {}
+    { init : ( model, Cmd msg )
+    , update : msg -> model -> ( model, Cmd msg )
+    , subscriptions : model -> Sub msg
+    , view : model -> Html msg
+    }
 
 
-toConcreteWidget : AbstractWidget model msg base -> Widget model msg
-toConcreteWidget widget =
-    { init = widget.init, update = widget.update, subscriptions = widget.subscriptions, view = widget.view }
+type alias Binding msg serializedType err =
+    { get : Sub (Result err serializedType)
+    , set : serializedType -> Cmd msg
+    }
 
 
-toConcreteWidgetWithFlags :
-    AbstractWidget model msg base
-    -> { init : flags -> ( model, Cmd msg )
-       , update : msg -> model -> ( model, Cmd msg )
-       , subscriptions : model -> Sub msg
-       , view : model -> Html msg
-       }
-toConcreteWidgetWithFlags widget =
-    { init = \_ -> widget.init, update = widget.update, subscriptions = widget.subscriptions, view = widget.view }
+type alias IBound msg serializedType err base =
+    { base | binding : Binding msg serializedType err }
+
+
+type alias ISelectable model msg base =
+    { base
+        | isSelected : model -> Bool
+        , selectMsg : msg
+        , unselectMsg : msg
+    }
+
+
+type alias IDecision msg base =
+    { base | confirmMsg : msg }
+
+
+type alias Index =
+    Int
+
+
+type alias IListBinding msg ty err base =
+    { base
+        | itemAdded : Sub (Result err ( Index, ty ))
+        , itemRemoved : Sub (Result err Index)
+        , addItem : Index -> ty -> Cmd msg
+        , removeItem : Index -> Cmd msg
+    }
+
+
+type alias ListBinding msg ty err =
+    IListBinding msg ty err (Binding msg (List ty) err)
+
+
+type alias IListBound msg serializedType err base =
+    { base | binding : ListBinding msg serializedType err }
+
+
+type alias Factory fromTy toTy =
+    fromTy -> toTy
 
 
 wrapUpdateWithCmd : (msg -> model -> model) -> msg -> model -> ( model, Cmd msg )
@@ -51,44 +82,3 @@ cmdOfMsg msg =
 emptySubscription : model -> Sub msg
 emptySubscription _ =
     Sub.none
-
-
-type alias ISelectable model msg base =
-    { base
-        | isSelected : model -> Bool
-        , selectMsg : msg
-        , unselectMsg : msg
-    }
-
-
-type alias SelectableWidget model msg =
-    ISelectable model msg (Widget model msg)
-
-
-type alias IDecision msg base =
-    { base
-        | confirmMsg : msg
-        , cancelMsg : msg
-    }
-
-
-type alias DecisionWidget model msg =
-    IDecision msg (Widget model msg)
-
-
-type alias Binding msg ty err =
-    { get : Sub (Result err ty)
-    , set : ty -> Cmd msg
-    }
-
-
-type alias Index =
-    Int
-
-
-type alias ListBinding msg ty err =
-    { itemAdded : Sub (Result err ( Index, ty ))
-    , itemRemoved : Sub (Result err Index)
-    , addItem : Index -> ty -> Cmd msg
-    , removeItem : Index -> Cmd msg
-    }
