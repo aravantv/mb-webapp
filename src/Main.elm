7271bce12b244e5750e720e0ea9fd1357768f671
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-01-21 00:26:04 +0100

    makes Main work with the new interface

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,28 @@
 module Main exposing (..)
 
 import Html
+import Json.Encode
+import SelectableText exposing (subscriptions)
+import Storage
 import Utils exposing (..)
+import Widget
 
 
--- import SelectableList
+--import SelectableList
 
 import SelectableText
-import Widget exposing (toConcreteWidget, toConcreteWidgetWithFlags)
+
+
+--import NewText
+
+import Widget
 import Storage
-import Json.Encode
 
 
 textBinding : Storage.Path -> Widget.Binding msg String ()
 textBinding p =
     { get =
-        -- TODO for now I have to return a Maybe type. In the future, take inspiration from WebSockets instead
+        -- TODO for now I have to return a Result type. In the future, take inspiration from WebSockets instead
         Storage.getStringSub
             (\( path, s ) ->
                 if path == p then
@@ -27,6 +34,8 @@ textBinding p =
     }
 
 
+
+{--
 listBinding : Storage.Path -> Widget.ListBinding msg String ()
 listBinding p =
     { itemAdded =
@@ -53,13 +62,25 @@ listBinding p =
     , removeItem = \i -> Storage.removeItemCmd (p ++ [ toString i ])
     }
 
+--}
+
 
 main : Program Never SelectableText.Model SelectableText.Msg
 main =
-    Html.program (toConcreteWidget <| SelectableText.createWidget (textBinding [ "0" ]))
+    let
+        widget =
+            SelectableText.createWidget (textBinding [ "0" ])
+    in
+        Html.program
+            { init = widget.init
+            , update = widget.update
+            , view = widget.view
+            , subscriptions = widget.subscriptions
+            }
 
 
 
+--  SelectableList.createListWidget (listBinding []) NewText.widget (SelectableText.createWidget (textBinding [ "0" ]))
 {--main : Program Never (SelectableList.Model SelectableText.Model) (SelectableList.Msg NewText.Msg SelectableText.Msg)
 main =
     Html.program (SelectableList.createListWidget NewText.widget SelectableText.widget)
