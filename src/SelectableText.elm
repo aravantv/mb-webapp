c3d62316d47e9089d45d954ad4d8af0c15e74c12
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-01-22 18:07:23 +0100

    adapts SelectableText to the new granularity of Path

--- src/SelectableText.elm
+++ src/SelectableText.elm
@@ -4,23 +4,19 @@ import Html exposing (Html, input, label, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onDoubleClick, onInput)
 import Utils exposing (..)
-import Widget exposing (Binding, ISelectable, FixedPathBinding, Widget, cmdOfMsg, wrapModelWithCmd)
+import Widget exposing (Binding, ISelectable, Path, Widget, cmdOfMsg, wrapWithNoCmd)
 
 
-createWidget : Binding Msg String err -> Widget.Path -> ISelectable Model Msg (Widget Model Msg)
-createWidget binding p =
-    let
-        concreteBinding =
-            binding p
-    in
-        { init = wrapModelWithCmd model
-        , update = update concreteBinding
-        , view = view
-        , subscriptions = subscriptions concreteBinding
-        , isSelected = .editMode
-        , selectMsg = UISelect
-        , unselectMsg = UIConfirm
-        }
+createWidget : Binding Msg String err -> ISelectable Model Msg (Widget Model Msg)
+createWidget binding =
+    { init = wrapWithNoCmd model
+    , update = update binding
+    , view = view
+    , subscriptions = subscriptions binding
+    , isSelected = .editMode
+    , selectMsg = UISelect
+    , unselectMsg = UIConfirm
+    }
 
 
 
@@ -56,35 +52,35 @@ type Msg
 -- in the long run, the parameter of ModelChange should not be a Maybe String but just a String
 
 
-update : FixedPathBinding Msg String err -> Msg -> Model -> ( Model, Cmd Msg )
+update : Binding Msg String err -> Msg -> Model -> ( Model, Path -> Cmd Msg )
 update binding msg model =
     case msg of
         UIChange newContent ->
-            ( { model | uiContent = newContent }, Cmd.none )
+            wrapWithNoCmd { model | uiContent = newContent }
 
         UIConfirm ->
             ( { model | editMode = False, content = model.uiContent }, binding.set model.uiContent )
 
         UICancel ->
-            ( { model | uiContent = model.content }, cmdOfMsg UIConfirm )
+            ( { model | uiContent = model.content }, always (cmdOfMsg UIConfirm) )
 
         UISelect ->
-            ( { model | editMode = True }, Cmd.none )
+            wrapWithNoCmd { model | editMode = True }
 
         NoOp ->
-            ( model, Cmd.none )
+            wrapWithNoCmd model
 
         ModelChange newContent ->
-            ( { model | uiContent = newContent, content = newContent }, Cmd.none )
+            wrapWithNoCmd { model | uiContent = newContent, content = newContent }
 
 
 
 -- SUBSCRIPTIONS
 
 
-subscriptions : FixedPathBinding Msg String err -> Model -> Sub Msg
+subscriptions : Binding Msg String err -> Model -> Sub (Path -> Msg)
 subscriptions binding model =
-    Sub.map (Result.withDefault NoOp << Result.map ModelChange) binding.get
+    Sub.map (\getter -> Result.withDefault NoOp << Result.map ModelChange << getter) binding.get
 
 
 
