81c268fd482deb7d1c7a1711d2602f7d06811cce
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-01-27 22:23:51 +0100

    reverts the path before sending to the ports

--- src/Storage.elm
+++ src/Storage.elm
@@ -1,11 +1,13 @@
-port module Storage exposing (..)
+port module Storage exposing (getStringSub, setStringCmd, itemAddedSub, itemRemovedSub, addItemCmd, removeItemCmd)
 
 import Platform.Sub
+import Widget
 
 
-{-| Paths are provided as a list of string: the root is the *last* element.
+{-| Storage paths are different than widget paths: they are ordered more intuitively,
+  i.e., the root is the *first* element. That requires reverting before calling the port though.
 -}
-type alias Path =
+type alias StoragePath =
     List String
 
 
@@ -18,21 +20,58 @@ type alias JsonString =
     String
 
 
-port getStringSub : (( Path, JsonString ) -> msg) -> Sub msg
+{-| Just reverts a path
+-}
+pathStorageOfPath : Widget.Path -> StoragePath
+pathStorageOfPath p =
+    List.reverse p
 
 
-port setStringCmd : ( Path, String ) -> Cmd msg
+port getStringSubPort : (( StoragePath, JsonString ) -> msg) -> Sub msg
 
 
-port itemAddedSub : (Path -> msg) -> Sub msg
+getStringSub : (( StoragePath, JsonString ) -> a) -> Sub a
+getStringSub msgBuilder =
+    getStringSubPort (\( p, s ) -> msgBuilder ( pathStorageOfPath p, s ))
 
 
-port itemRemovedSub : (Path -> msg) -> Sub msg
+port setStringCmdPort : ( StoragePath, String ) -> Cmd msg
+
+
+setStringCmd : ( Widget.Path, String ) -> Cmd msg
+setStringCmd ( p, s ) =
+    setStringCmdPort ( pathStorageOfPath p, s )
+
+
+port itemAddedSubPort : (StoragePath -> msg) -> Sub msg
+
+
+itemAddedSub : (StoragePath -> c) -> Sub c
+itemAddedSub msgBuilder =
+    itemAddedSubPort (msgBuilder << pathStorageOfPath)
+
+
+port itemRemovedSubPort : (StoragePath -> msg) -> Sub msg
+
+
+itemRemovedSub : (StoragePath -> c) -> Sub c
+itemRemovedSub msgBuilder =
+    itemRemovedSubPort (msgBuilder << pathStorageOfPath)
 
 
 {-| addItemCmd literally adds an item: it inserts an element at the given path but does not fill it in with any value!
 -}
-port addItemCmd : Path -> Cmd msg
+port addItemCmdPort : StoragePath -> Cmd msg
 
 
-port removeItemCmd : Path -> Cmd msg
+addItemCmd : Widget.Path -> Cmd msg
+addItemCmd =
+    addItemCmdPort << pathStorageOfPath
+
+
+port removeItemCmdPort : StoragePath -> Cmd msg
+
+
+removeItemCmd : Widget.Path -> Cmd msg
+removeItemCmd =
+    removeItemCmdPort << pathStorageOfPath
