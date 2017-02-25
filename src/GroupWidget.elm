7b2d4ece0894f0d346bfb43b1b87aaa57be20938
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-02-25 22:36:34 +0100

    make GroupWidget div or span

--- src/GroupWidget.elm
+++ src/GroupWidget.elm
@@ -8,8 +8,23 @@ type alias PathTransformer =
     Path -> Path
 
 
+type DivOrSpan
+    = Div
+    | Span
+
+
+htmlOfDivOrSpan : DivOrSpan -> List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
+htmlOfDivOrSpan dos =
+    case dos of
+        Div ->
+            div
+
+        Span ->
+            span
+
+
 type alias Parameters subModel1 msg1 factoryInput1 subModel2 msg2 factoryInput2 =
-    ( Widget subModel1 msg1 factoryInput1, PathTransformer, Widget subModel2 msg2 factoryInput2, PathTransformer )
+    ( Widget subModel1 msg1 factoryInput1, PathTransformer, Widget subModel2 msg2 factoryInput2, PathTransformer, DivOrSpan )
 
 
 createGroupWidget :
@@ -37,7 +52,7 @@ type alias Model subModel1 subModel2 =
 emptyModel :
     Parameters subModel1 subMsg1 factoryInput1 subModel2 subMsg2 factoryInput2
     -> Model subModel1 subModel2
-emptyModel ( wrappedWidget1, _, wrappedWidget2, _ ) =
+emptyModel ( wrappedWidget1, _, wrappedWidget2, _, _ ) =
     { wrappedModel1 = wrappedWidget1.initModel
     , wrappedModel2 = wrappedWidget2.initModel
     }
@@ -61,7 +76,7 @@ update :
     -> ( Model subModel1 subModel2, Cmd (Msg subMsg1 subMsg2 factoryInput1 factoryInput2) )
 update params msg model path =
     let
-        ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2 ) =
+        ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2, _ ) =
             params
     in
         case msg of
@@ -99,7 +114,7 @@ subscriptions :
     -> Model subModel1 subModel2
     -> Path
     -> Sub (Msg subMsg1 subMsg2 factoryInput1 factoryInput2)
-subscriptions ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2 ) model path =
+subscriptions ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2, _ ) model path =
     let
         sub1 =
             Sub.map DelegateToWidget1 <| wrappedWidget1.subscriptions model.wrappedModel1 (pathAdapter1 path)
@@ -118,8 +133,9 @@ view :
     Parameters subModel1 subMsg1 factoryInput1 subModel2 subMsg2 factoryInput2
     -> Model subModel1 subModel2
     -> Html (Msg subMsg1 subMsg2 factoryInput1 factoryInput2)
-view ( wrappedWidget1, _, wrappedWidget2, _ ) model =
-    div []
+view ( wrappedWidget1, _, wrappedWidget2, _, dos ) model =
+    htmlOfDivOrSpan dos
+        []
         [ Html.map DelegateToWidget1 (wrappedWidget1.view model.wrappedModel1)
         , Html.map DelegateToWidget2 (wrappedWidget2.view model.wrappedModel2)
         ]
