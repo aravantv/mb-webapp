db583b5358b29eace6e5d5d84e950656a229e515
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-02-25 23:04:17 +0100

    GroupWidget: record for params

--- src/GroupWidget.elm
+++ src/GroupWidget.elm
@@ -13,18 +13,13 @@ type DivOrSpan
     | Span
 
 
-htmlOfDivOrSpan : DivOrSpan -> List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
-htmlOfDivOrSpan dos =
-    case dos of
-        Div ->
-            div
-
-        Span ->
-            span
-
-
 type alias Parameters subModel1 msg1 factoryInput1 subModel2 msg2 factoryInput2 =
-    ( Widget subModel1 msg1 factoryInput1, PathTransformer, Widget subModel2 msg2 factoryInput2, PathTransformer, DivOrSpan )
+    { wrappedWidget1 : Widget subModel1 msg1 factoryInput1
+    , pathAdapter1 : PathTransformer
+    , wrappedWidget2 : Widget subModel2 msg2 factoryInput2
+    , pathAdapter2 : PathTransformer
+    , divOrSpan : DivOrSpan
+    }
 
 
 createGroupWidget :
@@ -52,9 +47,9 @@ type alias Model subModel1 subModel2 =
 emptyModel :
     Parameters subModel1 subMsg1 factoryInput1 subModel2 subMsg2 factoryInput2
     -> Model subModel1 subModel2
-emptyModel ( wrappedWidget1, _, wrappedWidget2, _, _ ) =
-    { wrappedModel1 = wrappedWidget1.initModel
-    , wrappedModel2 = wrappedWidget2.initModel
+emptyModel params =
+    { wrappedModel1 = params.wrappedWidget1.initModel
+    , wrappedModel2 = params.wrappedWidget2.initModel
     }
 
 
@@ -75,34 +70,30 @@ update :
     -> Path
     -> ( Model subModel1 subModel2, Cmd (Msg subMsg1 subMsg2 factoryInput1 factoryInput2) )
 update params msg model path =
-    let
-        ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2, _ ) =
-            params
-    in
-        case msg of
-            DelegateToWidget1 subMsg ->
-                let
-                    ( updatedModel1, cmd ) =
-                        wrappedWidget1.update subMsg model.wrappedModel1 (pathAdapter1 path)
-                in
-                    ( { model | wrappedModel1 = updatedModel1 }, Cmd.map DelegateToWidget1 cmd )
+    case msg of
+        DelegateToWidget1 subMsg ->
+            let
+                ( updatedModel1, cmd ) =
+                    params.wrappedWidget1.update subMsg model.wrappedModel1 (params.pathAdapter1 path)
+            in
+                ( { model | wrappedModel1 = updatedModel1 }, Cmd.map DelegateToWidget1 cmd )
 
-            DelegateToWidget2 subMsg ->
-                let
-                    ( updatedModel2, cmd ) =
-                        wrappedWidget2.update subMsg model.wrappedModel2 (pathAdapter2 path)
-                in
-                    ( { model | wrappedModel2 = updatedModel2 }, Cmd.map DelegateToWidget2 cmd )
+        DelegateToWidget2 subMsg ->
+            let
+                ( updatedModel2, cmd ) =
+                    params.wrappedWidget2.update subMsg model.wrappedModel2 (params.pathAdapter2 path)
+            in
+                ( { model | wrappedModel2 = updatedModel2 }, Cmd.map DelegateToWidget2 cmd )
 
-            Init ( fi1, fi2 ) ->
-                let
-                    initCmd1 =
-                        cmdOfMsg <| DelegateToWidget1 (wrappedWidget1.initMsg fi1)
+        Init ( fi1, fi2 ) ->
+            let
+                initCmd1 =
+                    cmdOfMsg <| DelegateToWidget1 (params.wrappedWidget1.initMsg fi1)
 
-                    initCmd2 =
-                        cmdOfMsg <| DelegateToWidget2 (wrappedWidget2.initMsg fi2)
-                in
-                    ( emptyModel params, Cmd.batch [ initCmd1, initCmd2 ] )
+                initCmd2 =
+                    cmdOfMsg <| DelegateToWidget2 (params.wrappedWidget2.initMsg fi2)
+            in
+                ( emptyModel params, Cmd.batch [ initCmd1, initCmd2 ] )
 
 
 
@@ -114,13 +105,13 @@ subscriptions :
     -> Model subModel1 subModel2
     -> Path
     -> Sub (Msg subMsg1 subMsg2 factoryInput1 factoryInput2)
-subscriptions ( wrappedWidget1, pathAdapter1, wrappedWidget2, pathAdapter2, _ ) model path =
+subscriptions params model path =
     let
         sub1 =
-            Sub.map DelegateToWidget1 <| wrappedWidget1.subscriptions model.wrappedModel1 (pathAdapter1 path)
+            Sub.map DelegateToWidget1 <| params.wrappedWidget1.subscriptions model.wrappedModel1 (params.pathAdapter1 path)
 
         sub2 =
-            Sub.map DelegateToWidget2 <| wrappedWidget2.subscriptions model.wrappedModel2 (pathAdapter2 path)
+            Sub.map DelegateToWidget2 <| params.wrappedWidget2.subscriptions model.wrappedModel2 (params.pathAdapter2 path)
     in
         Sub.batch [ sub1, sub2 ]
 
@@ -133,9 +124,17 @@ view :
     Parameters subModel1 subMsg1 factoryInput1 subModel2 subMsg2 factoryInput2
     -> Model subModel1 subModel2
     -> Html (Msg subMsg1 subMsg2 factoryInput1 factoryInput2)
-view ( wrappedWidget1, _, wrappedWidget2, _, dos ) model =
-    htmlOfDivOrSpan dos
-        []
-        [ Html.map DelegateToWidget1 (wrappedWidget1.view model.wrappedModel1)
-        , Html.map DelegateToWidget2 (wrappedWidget2.view model.wrappedModel2)
-        ]
+view params model =
+    let
+        divOrSpanHtml =
+            case params.divOrSpan of
+                Div ->
+                    div
+
+                Span ->
+                    span
+    in
+        divOrSpanHtml []
+            [ Html.map DelegateToWidget1 (params.wrappedWidget1.view model.wrappedModel1)
+            , Html.map DelegateToWidget2 (params.wrappedWidget2.view model.wrappedModel2)
+            ]
