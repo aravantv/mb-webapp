25c7c1203369b5667ab432bbf3c3ef1c9a4d21ec
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-05-13 22:39:19 +0200

    makes collectionBinding compile, but not state for int2string

--- src/CollectionBinding.elm
+++ src/CollectionBinding.elm
@@ -2,10 +2,11 @@ module CollectionBinding exposing (..)
 
 import Binding exposing (BindingResult)
 import ConstraintUtils exposing (Fixes(..), UnfulfillmentInfo)
-import DataID exposing (getItemIdentifier, itemOf)
+import Data
+import DataID exposing (DataID, getItemIdentifier, itemOf)
 import DataManager
 import DataType exposing (DataTypeSet)
-import EqualityConstraint exposing (DataID)
+import IndexMapping
 import Widget exposing (Widget, mapParamsSub, mapParamsUp)
 
 
@@ -23,55 +24,64 @@ type alias CollectionBindingUpdate collectionPath carriedValue msg =
     }
 
 
-type alias BindingItemAdded collectionPath carriedValue =
-    Sub (BindingResult ( collectionPath, carriedValue ))
+type alias BindingItemAdded collectionPath carriedValue msg =
+    (BindingResult ( collectionPath, carriedValue ) -> msg) -> Sub msg
 
 
-type alias BindingItemRemoved collectionPath =
-    Sub (BindingResult collectionPath)
+type alias BindingItemRemoved collectionPath msg =
+    (BindingResult collectionPath -> msg) -> Sub msg
 
 
-type alias CollectionBindingSubscriptions collectionPath carriedValue =
-    { itemAdded : BindingItemAdded collectionPath carriedValue
-    , itemRemoved : BindingItemRemoved collectionPath
+type alias CollectionBindingSubscriptions collectionPath carriedValue msg =
+    { itemAdded : BindingItemAdded collectionPath carriedValue msg
+    , itemRemoved : BindingItemRemoved collectionPath msg
     }
 
 
 type alias CollectionBinding collectionPath msg carriedValue =
     { addItem : BindingAddItem collectionPath carriedValue msg
     , removeItem : BindingRemoveItem collectionPath msg
-    , itemAdded : BindingItemAdded collectionPath carriedValue
-    , itemRemoved : BindingItemRemoved collectionPath
+    , itemAdded : BindingItemAdded collectionPath carriedValue msg
+    , itemRemoved : BindingItemRemoved collectionPath msg
     }
 
 
 type alias WidgetWithCollectionBinding collectionPath model msg carriedValue =
-    Widget (CollectionBindingUpdate collectionPath carriedValue msg) (CollectionBindingSubscriptions collectionPath carriedValue) model msg
+    Widget (CollectionBindingUpdate collectionPath carriedValue msg) (CollectionBindingSubscriptions collectionPath carriedValue msg) model msg
 
 
-type alias CollectionBindingWrapper collectionPath innerModel innerCarriedValue outerModel outerCarriedValue msg =
-    WidgetWithCollectionBinding collectionPath innerModel msg innerCarriedValue
-    -> WidgetWithCollectionBinding collectionPath outerModel msg outerCarriedValue
+type alias CollectionBindingAdapter collectionPath innerModel innerCarriedValue innerMsg outerModel outerCarriedValue outerMsg =
+    WidgetWithCollectionBinding collectionPath innerModel innerMsg innerCarriedValue
+    -> WidgetWithCollectionBinding collectionPath outerModel outerMsg outerCarriedValue
+
+
+type alias CollectionBindingWrapper collectionPath model msg carriedValue =
+    WidgetWithCollectionBinding collectionPath model msg carriedValue -> Widget () () model msg
+
+
+mapItemAdded :
+    (fromValue -> BindingResult toValue)
+    -> BindingResult ( collectionPath, fromValue )
+    -> BindingResult ( collectionPath, toValue )
+mapItemAdded out2in =
+    Binding.andThen (\( i, s ) -> Binding.map (\n -> ( i, n )) (out2in s))
 
 
 statelessWrapper :
     (innerCarriedValue -> BindingResult outerCarriedValue)
     -> (outerCarriedValue -> BindingResult innerCarriedValue)
-    -> CollectionBindingWrapper collectionPath model innerCarriedValue model outerCarriedValue msg
+    -> CollectionBindingAdapter collectionPath model innerCarriedValue msg model outerCarriedValue msg
 statelessWrapper in2out out2in =
     mapParamsUp
-        (\up ->
-            { addItem = \path -> Binding.andThen (up.addItem path) << in2out
-            , removeItem = up.removeItem
+        (\params ->
+            { addItem = \path -> Binding.andThen (params.addItem path) << in2out
+            , removeItem = params.removeItem
             }
         )
         << mapParamsSub
-            (\sub ->
-                { itemAdded =
-                    Sub.map
-                        (Binding.andThen (\( path, value ) -> Binding.map (\x -> ( path, x )) (out2in value)))
-                        sub.itemAdded
-                , itemRemoved = sub.itemRemoved
+            (\params ->
+                { itemAdded = \f -> params.itemAdded (f << mapItemAdded out2in)
+                , itemRemoved = params.itemRemoved
                 }
             )
 
@@ -89,39 +99,129 @@ type alias Index =
     Int
 
 
-listBinding : DataTypeSet -> DataID -> CollectionBinding Index msg carriedValue
+listBinding : DataTypeSet -> DataID -> CollectionBinding Index msg Data.Data
 listBinding dts boundId =
     { itemAdded =
-        DataManager.itemAddedSub dts
-            (\( id, maybeObj ) ->
-                case id |> itemOf boundId of
-                    Just i ->
-                        case maybeObj of
-                            Result.Ok obj ->
-                                Ok ( i, obj )
+        \f ->
+            DataManager.itemAddedSub dts
+                (\( id, maybeObj ) ->
+                    f
+                        (case id |> itemOf boundId of
+                            Just i ->
+                                case maybeObj of
+                                    Result.Ok obj ->
+                                        Binding.Ok ( i, obj )
 
-                            Result.Err err ->
-                                Err { unfulfillmentDescription = err, fixes = PossibleFixes [] }
+                                    Result.Err err ->
+                                        Binding.Err { unfulfillmentDescription = err, fixes = PossibleFixes [] }
 
-                    _ ->
-                        Binding.Irrelevant
-            )
+                            _ ->
+                                Binding.Irrelevant
+                        )
+                )
     , itemRemoved =
-        DataManager.itemRemovedSub
-            (\id ->
-                case id |> itemOf boundId of
-                    Just i ->
-                        Ok i
+        \f ->
+            DataManager.itemRemovedSub
+                (\id ->
+                    f
+                        (case id |> itemOf boundId of
+                            Just i ->
+                                Binding.Ok i
 
-                    _ ->
-                        Binding.Irrelevant
-            )
-    , addItem = \i m -> Ok <| DataManager.addItemCmd (getItemIdentifier boundId i) m
-    , removeItem = \i -> Ok <| DataManager.removeItemCmd (getItemIdentifier boundId i)
+                            _ ->
+                                Binding.Irrelevant
+                        )
+                )
+    , addItem = \i m -> Binding.Ok <| DataManager.addItemCmd (getItemIdentifier boundId i) m
+    , removeItem = \i -> Binding.Ok <| DataManager.removeItemCmd (getItemIdentifier boundId i)
     }
 
 
+type CollectionBindingMsg collectionPath carriedValue
+    = ItemAdded collectionPath
+    | ItemAddedButSkipped collectionPath
+    | ItemRemoved collectionPath
 
+
+intOfStringWrapper :
+    CollectionBindingWrapper collectionPath model msg String
+    -> CollectionBindingWrapper collectionPath model msg Int
+intOfStringWrapper wrapper w =
+    wrapper
+        (\id ->
+            let
+                cw =
+                    w id
+            in
+                { initModel = cw.initModel
+                , initMsg = cw.initMsg
+                , update =
+                    \msg model paramsUps ->
+                        cw.update msg
+                            model
+                            { addItem = \i n -> paramsUps.addItem i (toString n)
+                            , removeItem = paramsUps.removeItem
+                            }
+                , subscriptions =
+                    \model paramsSubs ->
+                        cw.subscriptions model
+                            { itemAdded = \f -> paramsSubs.itemAdded (f << mapItemAdded (Binding.ofResult << String.toInt))
+                            , itemRemoved = paramsSubs.itemRemoved
+                            }
+                , view = cw.view
+                }
+        )
+
+
+
+{--
+intOfStringWrapper :
+    CollectionBindingWrapper collectionPath model msg String
+    -> CollectionBindingWrapper collectionPath model msg Int
+intOfStringWrapper wrapper w id =
+    let
+        cw =
+            w id
+    in
+        wrapper
+            { initModel = ( cw.initModel, IndexMapping.empty )
+            , initMsg = cw.initMsg
+            , update =
+                \( wrappedMsg, bindingMsg ) ( model, idxMap ) paramsUps ->
+                    let
+                        ( newModel, cmd ) =
+                            cw.update wrappedMsg
+                                model
+                                { addItem =
+                                    \i n -> paramsUps.addItem i (toString n)
+                                , removeItem = paramsUps.removeItem
+                                }
+
+                        newIdxMap =
+                            case bindingMsg of
+                                Nothing ->
+                                    idxMap
+
+                                Just (ItemAdded idx) ->
+                                    IndexMapping.insert idxMap idx
+
+                                Just (ItemAddedButSkipped idx) ->
+                                    IndexMapping.insertButSkip idxMap idx
+
+                                Just (ItemRemoved idx) ->
+                                    IndexMapping.remove idxMap idx
+                    in
+                        ( ( newModel, newIdxMap ), cmd )
+            , subscriptions =
+                \( model, idxMap ) paramsSubs ->
+                    cw.subscriptions model
+                        { itemAdded = Sub.map (Binding.andThen (\( i, s ) -> Binding.map (\n -> ( i, n )) <| Binding.ofResult (String.toInt s))) paramsSubs.itemAdded
+                        , itemRemoved = paramsSubs.itemRemoved
+                        }
+            , view = cw.view
+            }
+            id
+--}
 {--
 type alias GenericCollectionBinding msg relativePath carriedValue =
     DataID
