dea586aeb0d353b023daa6e9a87d1df3d1c443ae
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-05-25 22:53:41 +0200

    adds an initial value at the widget level

--- src/widgets/Label.elm
+++ src/widgets/Label.elm
@@ -4,9 +4,9 @@ import Html exposing (Html, input, label, text)
 import Widget exposing (IDecision, TopWidget, Widget, doNothing)
 
 
-widget : Widget String () () Model Msg
-widget =
-    { init = \s -> ( s, Cmd.none )
+createWidget : String -> Widget () () Model Msg
+createWidget initValue =
+    { init = ( initValue, Cmd.none )
     , update = update
     , view = view
     , subscriptions = \_ -> ( Sub.none, () )
