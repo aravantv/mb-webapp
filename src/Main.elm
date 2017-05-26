c971f6cf6abc89132e407848cfe1f1d97bc1a1d0
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-05-27 00:55:14 +0200

    makes Main compile with List
    but not behaving properly

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,9 @@ module Main exposing (..)
 
 import Binding exposing (applyBinding, intOfStringWrapper, minus2Wrapper, stringOfIntWrapper, textBinding)
 import CircleWidget
+import CollectionBinding exposing (applyListBinding, listBinding)
+import Data
+import DataID exposing (DataID)
 import DataType exposing (ClassRef, DataType, DataTypeSet, FullDataType, Multiplicity, dataTypeSet)
 import GroupWidget exposing (..)
 import Label
@@ -38,65 +41,42 @@ listExampleWidget =
             GroupWidget.createWidget
                 { divOrSpan = Div
                 , wrappedWidget1 = Label.createWidget "List of stuff:"
-                , selector1 = identity
                 , wrappedWidget2 =
                     SelectableList.createWidget
                         { newItemWidget = NewText.createWidget ""
                         , itemWidget = SelectableText.createSelectableWidget Binding.textBinding
                         , factory = String
                         }
-                , selector2 = identity
                 }
-        , selector1 = identity
         , wrappedWidget2 =
             GroupWidget.createWidget
                 { divOrSpan = Div
                 , wrappedWidget1 = Label.createWidget "Only numbers:"
-                , selector1 = identity
                 , wrappedWidget2 =
                     SelectableList.createWidget
-                        { binding = (Binding.dataToStringListBinding >> Binding.stringToIntListBinding >> Binding.intToDataListBinding) (Binding.listBinding metamodel.dataTypeSet)
-                        , newItemWidget = NewText.widget
+                        {
+                        -- binding = (Binding.dataToStringListBinding >> Binding.stringToIntListBinding >> Binding.intToDataListBinding) (Binding.listBinding metamodel.dataTypeSet)
+                         newItemWidget = NewText.widget
                         , itemWidget = SelectableText.createSelectableWidget Binding.textBinding
                         , factory = String
                         }
-                , selector2 = identity
                 }
-        , selector2 = identity
         }
 --}
-{--
-formExampleWidget =
+
+
+listExampleWidget =
     GroupWidget.createWidget
         { divOrSpan = Div
-        , wrappedWidget1 =
-            GroupWidget.createWidget
-                { divOrSpan = Span
-                , wrappedWidget1 =
-                    CircleWidget.createWidget
-                        { wrappedWidget =
-                            SelectableText.widget Binding.textBinding
-                        , selector = identity
-                        }
-                , selector1 = identity
-                , wrappedWidget2 = Text.widget Binding.textBinding
-                , selector2 = identity
-                }
-        , selector1 = identity
+        , wrappedWidget1 = Label.createWidget "List of stuff:"
         , wrappedWidget2 =
-            GroupWidget.createWidget
-                { divOrSpan = Span
-                , wrappedWidget1 = Label.createWidget "Number+2:"
-                , selector1 = identity
-                , wrappedWidget2 =
-                    Text.widget
-                    -- (Binding.stringTransformerOfIntTransformer Binding.plus2Binding textBinding)
-                , selector2 = identity
-                }
-        , selector2 = identity
+            applyListBinding (listBinding [ DataID.Field "todos" ]) <|
+                SelectableList.createWidget
+                    { newItemWidget = NewText.createWidget ""
+                    , itemWidget = SelectableText.createSelectableWidget (\w id -> applyBinding (textBinding id) w)
+                    , factory = Data.String
+                    }
         }
-
---}
 
 
 formExampleWidget =
@@ -108,7 +88,7 @@ formExampleWidget =
                     { divOrSpan = Div
                     , wrappedWidget1 = Label.createWidget "n+2:"
                     , wrappedWidget2 =
-                        (applyBinding (textBinding [ DataType.Field "todos" ])
+                        (applyBinding (textBinding [ DataID.Field "todos" ])
                             << stringOfIntWrapper
                             << minus2Wrapper
                             << intOfStringWrapper
@@ -119,7 +99,7 @@ formExampleWidget =
             GroupWidget.createWidget
                 { divOrSpan = Div
                 , wrappedWidget1 = Label.createWidget "n:"
-                , wrappedWidget2 = applyBinding (textBinding [ DataType.Field "todos" ]) SelectableText.widget
+                , wrappedWidget2 = applyBinding (textBinding [ DataID.Field "todos" ]) SelectableText.widget
                 }
         }
 
@@ -127,6 +107,6 @@ formExampleWidget =
 main =
     let
         widget =
-            formExampleWidget
+            listExampleWidget
     in
         TimeTravel.program (makeTopWidget widget)
