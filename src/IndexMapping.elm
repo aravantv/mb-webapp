3c1c04b238b4183292d2eca99c6ff8d5baebe9d8
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-06-01 23:47:17 +0200

    improves readability

--- src/IndexMapping.elm
+++ src/IndexMapping.elm
@@ -5,7 +5,8 @@ type alias Index =
     Int
 
 
-{-| Every index in the list denotes an index which is skipped
+{-| Every pair (i,j) indicates that the object of index i in the original list,
+  corresponds to index j in the target list. We always have i >= j.
 -}
 type alias IndexMapping =
     List ( Index, Index )
@@ -16,18 +17,9 @@ empty =
     []
 
 
-insertButSkip : IndexMapping -> Index -> IndexMapping
-insertButSkip m i =
-    List.map
-        (\( j, k ) ->
-            if j < i then
-                ( j, k )
-            else
-                ( j + 1, k )
-        )
-        m
-
-
+{-| Returns the mapping after an object has been inserted at index [i] in the original list,
+  and is added as well in the target list (in general will not be at index [i]).
+-}
 insert : IndexMapping -> Index -> IndexMapping
 insert m i =
     case m of
@@ -41,6 +33,23 @@ insert m i =
                 ( i, k ) :: List.map (\( j, k ) -> ( j + 1, k + 1 )) m
 
 
+{-| Returns the mapping after an object has been inserted at index [i] in the original list,
+  but is skipped in the target list.
+-}
+insertButSkip : IndexMapping -> Index -> IndexMapping
+insertButSkip m i =
+    List.map
+        (\( j, k ) ->
+            if j < i then
+                ( j, k )
+            else
+                ( j + 1, k )
+        )
+        m
+
+
+{-| Returns the mapping after an object has been removed at index [i] from the original list.
+-}
 remove : IndexMapping -> Index -> IndexMapping
 remove m i =
     case m of
@@ -56,6 +65,8 @@ remove m i =
                 m
 
 
+{-| Returns the index in the target list of the object at index [i] in the original list.
+-}
 get : IndexMapping -> Index -> Maybe Index
 get m i =
     case m of
