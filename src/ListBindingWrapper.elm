d03c46ced991f3e1452a9b2f7dc4e15904c19851
Vincent Aravantinos <vincent.aravantinos@fortiss.org>
2017-06-15 12:49:11 +0200

    subscriptions cases defined in individual, documented, functions

--- src/ListBindingWrapper.elm
+++ src/ListBindingWrapper.elm
@@ -26,22 +26,103 @@ mapWrappedMsg f msg idxMap =
         ( f innerMsg, idxMap )
 
 
-type alias Model wrappedModel =
-    ( wrappedModel, IndexMapping )
+{-| Message in case the item at index [i] of the original list got removed.
+  We adapt the mapping and inform the wrapped widget, only if the item was present in the converted list.
+-}
+msgOfRemovedIndex : Index -> Msg (BindingResult Index)
+msgOfRemovedIndex i =
+    \idxMap ->
+        let
+            wrappedMsg =
+                case IndexMapping.get idxMap i of
+                    Just j ->
+                        Binding.Ok j
 
-
-getTranslatedIndex : IndexMapping -> Index -> BindingResult Index
-getTranslatedIndex idxMap i =
-    Binding.ofMaybe (IndexMapping.get idxMap i) "Index not found - please report"
+                    Nothing ->
+                        Binding.Irrelevant
+        in
+            ( wrappedMsg, IndexMapping.remove idxMap i )
 
 
 type alias Item carriedValue =
     ( Index, carriedValue )
 
 
-getTranslatedItem : IndexMapping -> Item carriedValue -> BindingResult (Item carriedValue)
-getTranslatedItem idxMap ( i, v ) =
-    getTranslatedIndex idxMap i |> Binding.map (\j -> ( j, v ))
+{-| Message in case the item at index [i] of value [outVal] get added to the original list.
+  We adapt the mapping and transmit the item to the wrapped widget only if conversion works out.
+-}
+msgOfMappedAddedItem : (outValue -> BindingResult inValue) -> Item outValue -> Msg (BindingResult (Item inValue))
+msgOfMappedAddedItem out2in ( i, outVal ) =
+    \idxMap ->
+        case out2in outVal of
+            Binding.Ok inVal ->
+                let
+                    ( newIdxMap, newIdx ) =
+                        IndexMapping.insertAndGet idxMap i
+                in
+                    ( Binding.Ok ( newIdx, inVal ), newIdxMap )
+
+            Binding.Err err ->
+                ( (Binding.Err err), IndexMapping.insertButSkip idxMap i )
+
+            Binding.Irrelevant ->
+                ( Binding.Irrelevant, IndexMapping.insertButSkip idxMap i )
+
+
+{-| Message in case the item at index [i] of the original list was modified into value [outVal].
+  We need to consider various cases depending if the item was before already present in the converted list or not.
+  If it was not, but the new value makes it present, then we do the same as for item addition.
+  Conversely if it was present, but the new value makes it absent, then we do the same as for item removal.
+  In other cases, we just change the values.
+-}
+msgOfMappedModifiedItem : (outValue -> BindingResult inValue) -> Item outValue -> Msg (BindingResult (Item inValue))
+msgOfMappedModifiedItem out2in ( i, outVal ) =
+    \idxMap ->
+        case ( out2in outVal, IndexMapping.get idxMap i ) of
+            ( Binding.Ok inVal, Just j ) ->
+                ( Binding.Ok ( j, inVal ), idxMap )
+
+            ( Binding.Ok inVal, Nothing ) ->
+                let
+                    ( newIdxMap, newIdx ) =
+                        IndexMapping.insertAndGet idxMap i
+                in
+                    ( Binding.Ok ( newIdx, inVal ), newIdxMap )
+
+            ( Binding.Err err, Nothing ) ->
+                ( Binding.Irrelevant, idxMap )
+
+            ( Binding.Err err, Just _ ) ->
+                ( Binding.Err err, IndexMapping.remove idxMap i )
+
+            ( Binding.Irrelevant, _ ) ->
+                ( Binding.Irrelevant, idxMap )
+
+
+{-| Message in case of receiving a full list.
+  We just go through all elements and add them to the list which we send to the wrapped widget,
+  only if the conversion works out.
+-}
+msgOfMappedFullList : (outValue -> BindingResult inValue) -> List outValue -> Msg (BindingResult (List inValue))
+msgOfMappedFullList out2in fullList =
+    \_ ->
+        let
+            itemOut2In outValue ( acc, idxMap, i ) =
+                case out2in outValue of
+                    Binding.Ok inValue ->
+                        ( inValue :: acc, IndexMapping.insert idxMap i, i + 1 )
+
+                    _ ->
+                        ( acc, IndexMapping.insertButSkip idxMap i, i + 1 )
+
+            ( newFullList, idxMap, _ ) =
+                List.foldr itemOut2In ( [], IndexMapping.empty, 0 ) fullList
+        in
+            ( Binding.Ok newFullList, idxMap )
+
+
+type alias Model wrappedModel =
+    ( wrappedModel, IndexMapping )
 
 
 type alias WrappedBoundListWidget wrappedModel wrappedMsg carriedValue =
@@ -67,103 +148,44 @@ makeListBindingWrapper :
     -> BoundListWidget innerModel innerMsg inCarriedValue
     -> WrappedBoundListWidget innerModel innerMsg outCarriedValue
 makeListBindingWrapper in2out out2in w =
-    let
-        msgOfAddedItem ( i, outVal ) =
-            \idxMap ->
-                case out2in outVal of
-                    Binding.Ok inVal ->
-                        let
-                            ( newIdxMap, newIdx ) =
-                                IndexMapping.insertAndGet idxMap i
-                        in
-                            ( Binding.Ok ( newIdx, inVal ), newIdxMap )
+    { init = ( ( modelOf w.init, IndexMapping.empty ), Cmd.map trivialMsg (cmdOf w.init) )
+    , update =
+        \msg ( model, idxMap ) ->
+            let
+                ( subMsg, newIdxMap ) =
+                    msg idxMap
 
-                    Binding.Err err ->
-                        ( (Binding.Err err), IndexMapping.insertButSkip idxMap i )
+                ( newModel, cmd, symbolicCmd ) =
+                    w.update subMsg model
 
-                    Binding.Irrelevant ->
-                        ( Binding.Irrelevant, IndexMapping.insertButSkip idxMap i )
+                newSymbolicCmd =
+                    mapCollectionPath (\i -> IndexMapping.retrieve newIdxMap i) symbolicCmd
+            in
+                ( ( newModel, newIdxMap ), Cmd.map trivialMsg cmd, mapSymbolicCmd in2out newSymbolicCmd )
+    , subscriptions =
+        \( model, _ ) ->
+            let
+                ( sub, symbolicSub ) =
+                    w.subscriptions model
 
-        msgOfModifiedItem ( i, outVal ) =
-            \idxMap ->
-                case ( out2in outVal, IndexMapping.get idxMap i ) of
-                    ( Binding.Ok inVal, Just j ) ->
-                        ( Binding.Ok ( j, inVal ), idxMap )
-
-                    ( Binding.Ok inVal, Nothing ) ->
-                        let
-                            ( newIdxMap, newIdx ) =
-                                IndexMapping.insertAndGet idxMap i
-                        in
-                            ( Binding.Ok ( newIdx, inVal ), newIdxMap )
-
-                    ( Binding.Err err, Nothing ) ->
-                        ( Binding.Err err, idxMap )
-
-                    ( Binding.Err err, Just _ ) ->
-                        ( Binding.Err err, IndexMapping.remove idxMap i )
-
-                    ( Binding.Irrelevant, _ ) ->
-                        ( Binding.Irrelevant, idxMap )
-
-        msgOfRemovedIndex i =
-            \idxMap ->
-                ( getTranslatedIndex idxMap i, IndexMapping.remove idxMap i )
-
-        msgOfFullList fullList =
-            \_ ->
-                let
-                    itemOut2In outValue ( acc, idxMap, i ) =
-                        case out2in outValue of
-                            Binding.Ok inValue ->
-                                ( inValue :: acc, IndexMapping.insert idxMap i, i + 1 )
-
-                            _ ->
-                                ( acc, IndexMapping.insertButSkip idxMap i, i + 1 )
-
-                    ( newFullList, idxMap, _ ) =
-                        List.foldr itemOut2In ( [], IndexMapping.empty, 0 ) fullList
-                in
-                    ( Binding.Ok newFullList, idxMap )
-    in
-        { init = ( ( modelOf w.init, IndexMapping.empty ), Cmd.map trivialMsg (cmdOf w.init) )
-        , update =
-            \msg ( model, idxMap ) ->
-                let
-                    ( subMsg, newIdxMap ) =
-                        msg idxMap
-
-                    ( newModel, cmd, symbolicCmd ) =
-                        w.update subMsg model
-
-                    newSymbolicCmd =
-                        mapCollectionPath (\i -> IndexMapping.retrieve newIdxMap i) symbolicCmd
-                in
-                    ( ( newModel, newIdxMap ), Cmd.map trivialMsg cmd, mapSymbolicCmd in2out newSymbolicCmd )
-        , subscriptions =
-            \( model, _ ) ->
-                let
-                    ( sub, symbolicSub ) =
-                        w.subscriptions model
-
-                    newSymbolicSub =
-                        { itemAdded =
-                            \resultItem ->
-                                mapWrappedMsg symbolicSub.itemAdded <| mapBindingRes msgOfAddedItem resultItem
-                        , itemModified =
-                            \resultItem ->
-                                mapWrappedMsg symbolicSub.itemModified <| mapBindingRes msgOfModifiedItem resultItem
-                        , itemRemoved =
-                            \resultIndex ->
-                                mapWrappedMsg symbolicSub.itemRemoved <| mapBindingRes msgOfRemovedIndex resultIndex
-                        , getFullList =
-                            \resultFullList ->
-                                mapWrappedMsg symbolicSub.getFullList <| mapBindingRes msgOfFullList resultFullList
-                        }
-                in
-                    ( Sub.map trivialMsg sub, newSymbolicSub )
-        , view = \( model, _ ) -> Html.map trivialMsg (w.view model)
-        }
+                newSymbolicSub =
+                    { itemAdded =
+                        \resultItem ->
+                            mapWrappedMsg symbolicSub.itemAdded <| mapBindingRes (msgOfMappedAddedItem out2in) resultItem
+                    , itemModified =
+                        \resultItem ->
+                            mapWrappedMsg symbolicSub.itemModified <| mapBindingRes (msgOfMappedModifiedItem out2in) resultItem
+                    , itemRemoved =
+                        \resultIndex ->
+                            mapWrappedMsg symbolicSub.itemRemoved <| mapBindingRes msgOfRemovedIndex resultIndex
+                    , getFullList =
+                        \resultFullList ->
+                            mapWrappedMsg symbolicSub.getFullList <| mapBindingRes (msgOfMappedFullList out2in) resultFullList
+                    }
+            in
+                ( Sub.map trivialMsg sub, newSymbolicSub )
+    , view = \( model, _ ) -> Html.map trivialMsg (w.view model)
+    }
 
 
 stringOfIntBindingWrapper :
